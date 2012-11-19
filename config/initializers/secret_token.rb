f1e76ef11be31fcc10d76c07d93c2c972b76783c
ClaudiaMorgana <claudia_morgana@hotmail.com>
2012-11-19 18:15:12 -0200

    teste

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sadn::Application.config.secret_token = 'f813ddeba55b8124562b73df262e0b0e88896c4fd98cc985fdfb72b90b26136228765439f3c63312fe71a2def5111ca4de982edb51d659b79ea2553dc009d147'
